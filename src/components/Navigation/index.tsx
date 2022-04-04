8e06d76d7877af8316a4929c9c901075397dea40
developjik <developjik@gmail.com>
2022-04-05 00:57:07 +0900

    refactor: Navigation Bar 수정

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,10 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
+import useMediaQuery from 'hooks/useMediaQuery';
 import styled from 'styled-components';
-import { MENUS, LOGINS } from 'common/constant';
+import { Link } from 'common/style';
+import TabletNavigation from 'components/Navigation/TabletNavigation';
+import DesktopNavigation from 'components/Navigation/DesktopNavigation';
 
 const Navbar = styled.nav`
   position: fixed;
@@ -12,57 +16,17 @@ const Navbar = styled.nav`
   border-bottom: 1px solid #eee;
 `;
 
-const Link = styled.a`
-  text-decoration: none;
-  color: #000;
-`;
-
 const Logo = styled.div``;
 
-const NavItems = styled.ul`
-  display: flex;
-  border: 1px solid #eee;
-  list-style: none;
-
-  @media (max-width: 1024px) {
-    display: none;
-  }
-`;
-
-const NavItem = styled.li`
-  & + & {
-    margin-left: 1em;
-  }
-`;
-
-const MobileMenu = styled.div`
-  display: block;
-  @media (min-width: 1024px) {
-    display: none;
-  }
-`;
-
 function Navigation() {
+  const matches = useMediaQuery('(min-width: 1024px)');
+
   return (
     <Navbar>
       <Logo>
         <Link href="/">LOGO</Link>
       </Logo>
-      <NavItems>
-        {MENUS.map(menu => (
-          <NavItem key={menu}>
-            <Link href="/">{menu}</Link>
-          </NavItem>
-        ))}
-      </NavItems>
-      <NavItems>
-        {LOGINS.map(login => (
-          <NavItem key={login}>
-            <Link href="/">{login}</Link>
-          </NavItem>
-        ))}
-      </NavItems>
-      <MobileMenu>menu</MobileMenu>
+      {matches ? <DesktopNavigation /> : <TabletNavigation />}
     </Navbar>
   );
 }
