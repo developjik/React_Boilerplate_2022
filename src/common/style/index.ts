dc313f2a49cf18a33f1caa59f04051242f600031
developjik <developjik@gmail.com>
2022-04-05 00:54:56 +0900

    feat: 공통 스타일 추가

---
+++ src/common/style/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+const Link = styled.a`
+  text-decoration: none;
+  color: #000;
+`;
+
+export { Link };
