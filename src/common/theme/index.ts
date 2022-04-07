2594a58d819cf312b052d88a912e11e7a98d7d1a
developjik <developjik@gmail.com>
2022-04-08 00:59:35 +0900

    feat: common theme 추가

---
+++ src/common/theme/index.ts
@@ -0,0 +1,13 @@
+import { ThemeInterface } from 'common/interface';
+
+const lightTheme: ThemeInterface = {
+  bg: '#eff0f5',
+  text: '#202124',
+};
+
+const darkTheme: ThemeInterface = {
+  bg: '#202124',
+  text: '#eff0f5',
+};
+
+export { lightTheme, darkTheme };
